4f779fa1ff2582029bcfbce32aa270c085a11a40
TrvMarco <trevisanello.marco@gmail.com>
2022-05-05 17:44:21 +0200

    aggiunto stampa degli array nei div

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,23 +3,30 @@
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
 // Seleziono i div tramite id
-const firstDiv = document.getElementById("uno")
-const secondDiv = document.getElementById("due")
+const firstDiv = document.getElementById("uno");
+const secondDiv = document.getElementById("due");
 // dò il colore al testo per ogni div
-firstDiv.style.color = "red"
-secondDiv.style.color = "green"
+firstDiv.style.color = "red";
+secondDiv.style.color = "green";
 
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
-const array = [10,21,12,34,77,22,175,93,43]
+const array = [10,21,12,34,77,22,175,93,43];
+
+const arrayPari = [];
+const arrayDispari = [];
 
 for (let i = 0; i < array.length; i++){
     console.log(array[i])
     if(array[i] % 2 == 0){
-        firstDiv.innerHTML = array[i]
+        arrayPari.push(array[i]);
     }else{
-        secondDiv.innerHTML = array[i]
+        arrayDispari.push(array[i]);
     }
 }
 
+console.log(arrayPari)
+console.log(arrayDispari)
 
+firstDiv.append(arrayPari)
+secondDiv.append(arrayDispari)
