b7ae7da16e0f359e87496730496aee8b8fcf1f7b
TrvMarco <trevisanello.marco@gmail.com>
2022-05-05 15:26:15 +0200

    bozza

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,1 +1,14 @@
-// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// Crea un array vuoto
+const empityArray = []
+//  e chiedi all’utente un numero da inserire nell’array.
+const userNumberImput = Number(prompt("Inserisci un numero"))
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// empityArray.push(userNumberImput)
+// console.log(empityArray)
+
+// do{
+//     const userNumberImput = Number(prompt("Inserisci un numero"))
+//     empityArray.push(userNumberImput)
+// }while{
+
+// }
