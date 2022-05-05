c2643b6ab53d45e610247483ec28d9e26b4bef89
TrvMarco <trevisanello.marco@gmail.com>
2022-05-05 16:41:43 +0200

    aggiunto ciclo

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,17 +2,10 @@
 const n = Number(prompt("inserisci un numero"));
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
 
-
-// array formato da numeri casuali tra 1 e 100.
-const arrayMain = []
-
-for (i = 0; i < 10; i++){
-    arrayMain.push(Math.floor(Math.random() * 100) + 1)
+for(let a = 0; a < n; a++ ){
+    const arrayMain = []
+    for (let i = 0; i < 10; i++){
+        arrayMain.push(Math.floor(Math.random() * 100) + 1)
+    }
+    console.log(arrayMain)
 }
-
-console.log(arrayMain)
-
-
-
-
-
